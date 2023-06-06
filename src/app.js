1beb3aa05c90ebe7d0bf25a4d58bd0222c240b79
liudmyla2 <131667474+liudmyla2@users.noreply.github.com>
2023-06-06 12:04:01 +0300

    Added JS

---
+++ src/app.js
@@ -0,0 +1,43 @@
+function formatDate(timestamp) {
+    let date = newDate(timestamp);
+    let hours = date.getHours();
+    if (minutes < 10) {
+        hours = "0" + hours;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    let day = days[date.getDay()];
+    return day + " " + hours + ":" + minutes;
+}
+
+
+
+function displayTemperature(response) {
+    console.log(response.data);
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    let iconElement = document.querySelector("#icon");
+    let dateElement = document.querySelector("#date");
+    
+    temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+    cityElement.innerHTML = response.data.city;
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = response.data.temperature.humidity;
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+    iconElement.setAttribute("src", response.data.condition.icon_url);
+    iconElement.setAttribute("alt", response.data.condition.description);
+    dateElement.innerHTML = formatDate(response.data.time * 1000)
+    
+}
+
+
+let apiKey = "tfb8e2a7d0b2e327445f129854d65o00";
+let apiUrl = "https://api.shecodes.io/weather/v1/current?query=Lisbon&key=tfb8e2a7d0b2e327445f129854d65o00&units=metric";
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
