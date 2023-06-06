0b6a71848b96d65865e1aa9e16c9ec7f7a4bc253
liudmyla2 <131667474+liudmyla2@users.noreply.github.com>
2023-06-06 21:06:36 +0300

    Unit conversion added

--- src/app.js
+++ src/app.js
@@ -1,22 +1,23 @@
 function formatDate(timestamp) {
-    let date = newDate(timestamp);
+    let date = new Date(timestamp);
     let hours = date.getHours();
-    if (minutes < 10) {
-        hours = "0" + hours;
+    if (hours < 10) {
+        hours = `0${hours}`;
     }
     let minutes = date.getMinutes();
     if (minutes < 10) {
-        minutes = "0" + minutes;
+        minutes = `0${minutes}`;
     }
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
     let day = days[date.getDay()];
-    return day + " " + hours + ":" + minutes;
+    return `${day} ${hours}:${minutes}`;
 }
 
 
 
 function displayTemperature(response) {
-    console.log(response.data);
+
+    
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#description");
@@ -24,8 +25,11 @@ function displayTemperature(response) {
     let windElement = document.querySelector("#wind");
     let iconElement = document.querySelector("#icon");
     let dateElement = document.querySelector("#date");
+
+    celsiusTemperature = response.data.temperature.current;
     
-    temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+    
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
     cityElement.innerHTML = response.data.city;
     descriptionElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML = response.data.temperature.humidity;
@@ -33,11 +37,49 @@ function displayTemperature(response) {
     iconElement.setAttribute("src", response.data.condition.icon_url);
     iconElement.setAttribute("alt", response.data.condition.description);
     dateElement.innerHTML = formatDate(response.data.time * 1000)
+   
     
 }
 
+function search(city) {
+    let apiKey = "tfb8e2a7d0b2e327445f129854d65o00";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
+}
 
-let apiKey = "tfb8e2a7d0b2e327445f129854d65o00";
-let apiUrl = "https://api.shecodes.io/weather/v1/current?query=Lisbon&key=tfb8e2a7d0b2e327445f129854d65o00&units=metric";
-console.log(apiUrl);
-axios.get(apiUrl).then(displayTemperature);
+
+
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+
+function displayFahrenheitTemperature(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+    let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+    temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displayCelsiusTemperature(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
+
+
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+search("New York");
+
