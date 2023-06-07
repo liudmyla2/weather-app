c86dfd9dd40de9dda61e2ec28ceac92c69ce90c6
liudmyla2 <131667474+liudmyla2@users.noreply.github.com>
2023-06-07 20:52:06 +0300

    Created columns using JS

--- src/app.js
+++ src/app.js
@@ -13,6 +13,33 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+    let forecastElement = document.querySelector("#forecast");
+
+    let forecastHTML = `<div class="row">`;
+    let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
+    days.forEach(function (day) {
+         forecastHTML = forecastHTML + `
+            <div class="col-2">
+                <div class="weather-forecast-date">
+                ${day}
+                </div>
+                <img src="" alt="" width="42">
+                <div class="weather-forecast-temperatures">
+                    <span class="weather-forecast-temperature-max">
+                        18
+                    </span> 
+                    <span class="weather-forecast-temperature-min">
+                        12
+                    </span>
+                </div>
+            </div>
+    `;  
+    })
+    
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+}
 
 
 function displayTemperature(response) {
@@ -87,4 +114,5 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
+displayForecast();
 
