1c3d7e3ad0dc912e7a96480b4109fcf68478c0c0
liudmyla2 <131667474+liudmyla2@users.noreply.github.com>
2023-06-08 21:04:28 +0300

    Added a function to display data using API

--- app.js
+++ app.js
@@ -13,40 +13,30 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`;
 }
 
-function formatDay(timestamp) {
-    let date = new Date(timestamp * 1000);
-    let day = date.getDay();
-    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
-
-    return days[day];
-
-}
-
 function displayForecast(response) {
-    let forecast = response.data.daily;
+    console.log(response.data.daily);
     
     let forecastElement = document.querySelector("#forecast");
 
     let forecastHTML = `<div class="row">`;
-    forecast.forEach(function (forecastDay, index) {
-        if (index < 6) {
+    let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
+    days.forEach(function (day) {
          forecastHTML = forecastHTML + `
             <div class="col-2">
                 <div class="weather-forecast-date">
-                ${formatDay(forecastDay.time)}
+                ${day}
                 </div>
-                <img src="${forecastDay.condition.icon_url}" alt="${forecastDay.condition.description}" width="42">
+                <img src="" alt="" width="42">
                 <div class="weather-forecast-temperatures">
                     <span class="weather-forecast-temperature-max">
-                        ${Math.round(forecastDay.temperature.maximum)}°
+                        18
                     </span> 
                     <span class="weather-forecast-temperature-min">
-                        ${Math.round(forecastDay.temperature.minimum)}°
+                        12
                     </span>
                 </div>
             </div>
     `;  
-        }
     })
     
     forecastHTML = forecastHTML + `</div>`;
